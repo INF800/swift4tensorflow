be02bc4222d6e2ed566731026b31ebcdea9d1e8f
Asapanna Rakesh <45640029+rakesh4real@users.noreply.github.com>
2019-11-27 13:01:19 +0530

    optimal q added

--- 04-data-structures/DataStructures/Sources/DataStructures/Queue.swift
+++ 04-data-structures/DataStructures/Sources/DataStructures/Queue.swift
@@ -46,7 +46,58 @@ public struct SimpleQueue<T>{
   }
 }
 
-// ================================================================
-// ================================================================
+// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
+
+// Optimal Queue
+
+public struct Queue<T>{
+  public var array = Array<T?>()
+  var head: Int = 0
+
+  public init(elements: Array<T>){
+    array = elements
+  }
+
+  public init() {  
+    // for empty Queue initialisation
+  }
+
+  public var count: Int{
+    return array.count - head
+  }
+
+  public var isEmpty: Bool{
+    return count==0
+  }
+
+  public func show(){
+    print(array)
+  }
+
+  public mutating func enqueue(_ element: T){
+    array.append(element)
+  }
+
+  public mutating func dequeue() -> T?{
+    
+    // without `&& head < array.count` no error 
+    guard !isEmpty && head < array.count else{ return nil}
+
+    let dequeuedElement = array[head]
+    array[head] = nil
+    head = head + 1
+
+    let max_limit_without_trimming = 10
+    let percent_of_nils = Double(head) / Double(array.count)
+    
+    if array.count > max_limit_without_trimming && percent_of_nils > 0.3 {
+      array.removeFirst(head)
+      head = 0
+    }
+
+    return dequeuedElement
+  }
+}
 
 
