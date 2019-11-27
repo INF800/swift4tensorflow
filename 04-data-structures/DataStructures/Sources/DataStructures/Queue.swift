3e0412113bed94962798fd9f230dd4c2f7ac4efa
Asapanna Rakesh <45640029+rakesh4real@users.noreply.github.com>
2019-11-27 14:45:05 +0530

    Add examples

--- 04-data-structures/DataStructures/Sources/DataStructures/Queue.swift
+++ 04-data-structures/DataStructures/Sources/DataStructures/Queue.swift
@@ -42,7 +42,7 @@ public struct SimpleQueue<T>{
   }
 
   public var last: T?{
-    return array[array.count]
+    return array[array.count] // `array.last` instead produce error
   }
 
   public func show(){
@@ -111,7 +111,7 @@ public struct Queue<T>{
   }
 
   public var last: T?{
-    return array[array.count]
+    return array[array.count] // `array.last` instead produce error
   }
 
   public func show(){
